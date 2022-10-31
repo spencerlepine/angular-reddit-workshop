9b1a70540fa288f42cd5c5cd3c41a2645a00c7da
Spencer Lepine <spencerlepine26@gmail.com>
2022-10-31 15:53:58 -0700

    attempt to fix outdated dependencies

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { SignupRequestPayload } from './signup-request.payload';
+import { AuthService } from '../shared/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  signupRequestPayload: SignupRequestPayload;
+  signupForm!: FormGroup;
+
+  constructor(private authService: AuthService, private router: Router,
+    private toastr: ToastrService) {
+    this.signupRequestPayload = {
+      username: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  signup() {
+    // @ts-ignore: Object is possibly 'null'.
+    this.signupRequestPayload.email = this.signupForm.get('email').value;
+    // @ts-ignore: Object is possibly 'null'.
+    this.signupRequestPayload.username = this.signupForm.get('username').value;
+    // @ts-ignore: Object is possibly 'null'.
+    this.signupRequestPayload.password = this.signupForm.get('password').value;
+
+    this.authService.signup(this.signupRequestPayload)
+      .subscribe(() => {
+        this.router.navigate(['/login'],
+          { queryParams: { registered: 'true' } });
+      }, (error: any) => {
+        console.log(error);
+        this.toastr.error('Registration Failed! Please try again');
+      });
+  }
+}
